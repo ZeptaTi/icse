1faf8e425ea19f531a2401b15975a7ed617a3668
elncosta <elncosta@gmail.com>
2017-08-23 13:49:12 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 --curl -sL https://raw.githubusercontent.com/ZeptaTi/icse/master/install-opencv.sh | sudo -E bash -
 
 -- Atualiza e instala o Node
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt install nodejs
+--curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+--sudo apt install nodejs
 
 cd ~/home/pi
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.46.tar.gz
