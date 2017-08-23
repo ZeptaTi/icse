bec52494cbdedfe92d8dfba7e31f1669787a16c6
elncosta <elncosta@gmail.com>
2017-08-23 13:48:11 -0300

    first commit

--- setup.sh
+++ setup.sh
@@ -1,22 +1,24 @@
 
-curl -sL https://raw.githubusercontent.com/ZeptaTi/icse/master/install-opencv.sh | sudo -E bash -
+-- Instala o opencv
+--curl -sL https://raw.githubusercontent.com/ZeptaTi/icse/master/install-opencv.sh | sudo -E bash -
 
--- Em qualquer diretorio
+-- Atualiza e instala o Node
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt install nodejs
 
--- No diretorio base /home/pi
+cd ~/home/pi
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.46.tar.gz
 tar xvzf bcm2835-1.46.tar.gz
 cd bcm2835-1.46/
 ./configure
 make
 sudo make install
- 
--- No diretorio do remoto /home/pi/remoto (criar):
+
+mkdir remoto 
 npm install node-dht-sensor
 npm install request
-Copiar este arquivo para /home/pi/remoto
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.46.tar.gz
+
 
 -- Executar (estando no diretorio remoto)
 sudo node app.js
