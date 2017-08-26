154085b797790cd56c88cc8d2212482e5a6de08f
elncosta <elncosta@gmail.com>
2017-08-26 14:36:03 -0300

    1

--- setup.sh
+++ setup.sh
@@ -18,6 +18,19 @@ function title {
 }
 
 # ---------------------------------------------------------------
+title "Instala CMAKE"
+sudo apt-get install cmake -y
+
+# ---------------------------------------------------------------
+title "Instala lib hardware BCM"
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.46.tar.gz
+tar xvzf bcm2835-1.46.tar.gz
+cd bcm2835-1.46/
+./configure
+make
+sudo make install
+
+# ---------------------------------------------------------------
 title "Update repositorio node"
 TEMP_NODE_SCRIPT="/home/pi/nodeSetup.sh"
 curl -sL https://deb.nodesource.com/setup_8.x > $TEMP_NODE_SCRIPT
@@ -30,55 +43,37 @@ title "Install node"
 sudo apt install nodejs
 
 # ---------------------------------------------------------------
-title "Dependencias remoto"
+title "Dependencias node do remoto"
 npm install node-dht-sensor
 npm install request
 
-cd \home\pi
-
 # ---------------------------------------------------------------
-title "Instala lib hardware BCM"
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.46.tar.gz
-tar xvzf bcm2835-1.46.tar.gz
-cd bcm2835-1.46/
-./configure
-make
-sudo make install
-
-# Baixa o remoto
-title "Dependencias remoto"
-cd remoto
+title "Baixa remoto"
+mkdir \home\pi\remoto 
+mkdir \home\pi\Eventos
+cd \home\pi\remoto
 wget https://raw.githubusercontent.com/ZeptaTi/icse/master/app.js
 
-# Cria os diretorios
-cd \home\pi
-mkdir remoto 
-mkdir Eventos
-
-# Instala o CMAKE
-title "Install CMAKE"
-sudo apt-get install cmake -y
-
-# Instala o opencv
-#title "Install OPENCV"
-#curl -sL https://raw.githubusercontent.com/ZeptaTi/icse/master/install-opencv.sh | sudo -E bash -
-
-# Instala o contador
+# ---------------------------------------------------------------
+title "Baixa contador"
 mkdir /home/pi/contador
 cd /home/pi/contador
 wget https://raw.githubusercontent.com/ZeptaTi/icse/master/contador
 chmod +x contador
 wget https://raw.githubusercontent.com/ZeptaTi/icse/master/calib.cfg
 
-# Adiciona inicialização automática
+# ---------------------------------------------------------------
+title "Adicionando inicialização automática"
 echo "@lxterminal -e node /home/pi/remoto/app.js" | sudo tee -a ~/.config/lxsession/LXDE-pi/autostart
-
-# remove os arquivo baixados
+echo "OK"
+# ---------------------------------------------------------------
+title "Removendo arquivos temporários"
+rm $TEMP_NODE_SCRIPT
 rm /home/pi/bcm2835-1.46.tar.gz
 rm /home/pi/opencv-3.1.0.zip
 rm /home/pi/opencv_contrib-3.1.0.zip
+echo "OK"
 
-
-
-
-
+# ---------------------------------------------------------------
+#title "Instala OPENCV"
+#curl -sL https://raw.githubusercontent.com/ZeptaTi/icse/master/install-opencv.sh | sudo -E bash -
