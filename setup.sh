e9c5ad06f10c4ffd9990cd387b2c944985929bf0
elncosta <elncosta@gmail.com>
2017-08-26 14:22:36 -0300

    1

--- setup.sh
+++ setup.sh
@@ -9,17 +9,27 @@
 # curl -sL  https://raw.githubusercontent.com/ZeptaTi/icse/master/setup.sh | sudo -E bash -
 #
 
-# Atualiza e instala o Node
+# ---------------------------------------------------------------
+title "Update repositorio node"
 TEMP_NODE_SCRIPT="/home/pi/nodeSetup.sh"
 curl -sL https://deb.nodesource.com/setup_8.x > $TEMP_NODE_SCRIPT
+echo "exit" >> $TEMP_NODE_SCRIPT
 chmod +x $TEMP_NODE_SCRIPT
 bash -c $TEMP_NODE_SCRIPT
 
+# ---------------------------------------------------------------
+title "Install node"
 sudo apt install nodejs
+
+# ---------------------------------------------------------------
+title "Dependencias remoto"
+npm install node-dht-sensor
+npm install request
 
 cd \home\pi
 
-# Instala as lib do hardware (necessário para ler o sensor de temp)
+# ---------------------------------------------------------------
+title "Instala lib hardware BCM"
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.46.tar.gz
 tar xvzf bcm2835-1.46.tar.gz
 cd bcm2835-1.46/
@@ -27,23 +37,22 @@ cd bcm2835-1.46/
 make
 sudo make install
 
+# Baixa o remoto
+title "Dependencias remoto"
+cd remoto
+wget https://raw.githubusercontent.com/ZeptaTi/icse/master/app.js
+
 # Cria os diretorios
 cd \home\pi
 mkdir remoto 
 mkdir Eventos
 
-# Baixa o remoto
-cd remoto
-wget https://raw.githubusercontent.com/ZeptaTi/icse/master/app.js
-
-# Instala dependencias do remoto
-npm install node-dht-sensor
-npm install request
-
 # Instala o CMAKE
+title "Install CMAKE"
 sudo apt-get install cmake -y
 
 # Instala o opencv
+#title "Install OPENCV"
 #curl -sL https://raw.githubusercontent.com/ZeptaTi/icse/master/install-opencv.sh | sudo -E bash -
 
 # Instala o contador
@@ -60,6 +69,15 @@ echo "@lxterminal -e node /home/pi/remoto/app.js" | sudo tee -a ~/.config/lxsess
 rm /home/pi/bcm2835-1.46.tar.gz
 rm /home/pi/opencv-3.1.0.zip
 rm /home/pi/opencv_contrib-3.1.0.zip
+
+
+function title {
+    echo "---------------------------------------------------------------------------"
+    echo "---------------------------------------------------------------------------"
+    echo $1
+    echo "---------------------------------------------------------------------------"
+    echo "---------------------------------------------------------------------------"
+}
 
 
 
